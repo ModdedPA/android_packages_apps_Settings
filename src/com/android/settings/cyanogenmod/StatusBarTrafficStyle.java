a7d35411172833b8d55ff0e6ae973f03e3714d7c
TheMrcool212 <munirmohamadrani@gmail.com>
2013-09-29 19:52:30 +0800

    Network indicator custom color fix

--- src/com/android/settings/cyanogenmod/StatusBarTrafficStyle.java
+++ src/com/android/settings/cyanogenmod/StatusBarTrafficStyle.java
@@ -43,7 +43,7 @@ import com.android.settings.widget.SeekBarPreference;
  * Using the original CyanogenMod Color Picker instead of margaritov used 
  * in Slim roms
  */
-import com.android.settings.cyanogenmod.colorpicker.ColorPickerPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class StatusBarTrafficStyle extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {

8530da0d89898e0eb9b62c0e3e160ff84d730d61
TheMrcool212 <munirmohamadrani@gmail.com>
2013-10-17 06:21:33 +0800

    Remove Display Settings line
    We don't have custom display settings like CM

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -69,7 +69,6 @@ public class BootReceiver extends BroadcastReceiver {
         }
 
         /* Restore the hardware tunable values */
-        DisplaySettings.restore(ctx);
         LocationSettings.restore(ctx);
     }
 

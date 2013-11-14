025b407ff45d45d9f272e2c6426a278af1517553
TheMrcool212 <munirmohamadrani@gmail.com>
2013-11-14 13:46:24 +0800

    Alternate default app picker (2/2)
    Source: https://github.com/temasek/android_packages_apps_Settings/commit/40c81916536cbd400bae8d4744bb48d6cb008eb2

--- src/com/android/settings/moddedpa/ModdedPASettings.java
+++ src/com/android/settings/moddedpa/ModdedPASettings.java
@@ -17,17 +17,38 @@ import com.android.settings.SettingsPreferenceFragment;
 public class ModdedPASettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";  
+
+    private CheckBoxPreference mUseAltResolver;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.moddedpa_settings);
 
+        mUseAltResolver = (CheckBoxPreference) findPreference(PREF_USE_ALT_RESOLVER);
+        mUseAltResolver.setChecked(Settings.System.getInt(
+                getActivity().getContentResolver(),
+                Settings.System.ACTIVITY_RESOLVER_USE_ALT, 0) == 1); 
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mUseAltResolver) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.ACTIVITY_RESOLVER_USE_ALT,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {

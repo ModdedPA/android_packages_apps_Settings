15e3f2da38fe40043121e5478d311e6278e94bcc
TheMrcool212 <munirmohamadrani@gmail.com>
2013-11-20 14:00:27 +0800

    Fix crashes

--- src/com/android/settings/moddedpa/ModdedPASettings.java
+++ src/com/android/settings/moddedpa/ModdedPASettings.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (C) 2013 ModdedPA Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings.moddedpa;
 
 import android.app.ActivityManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -10,22 +27,35 @@ import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class ModdedPASettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+import java.util.ArrayList;
+
+public class ModdedPASettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";  
 
     private CheckBoxPreference mUseAltResolver;
+
+    private Context mContext;
+
+    private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
+    private final ArrayList<CheckBoxPreference> mResetCbPrefs
+            = new ArrayList<CheckBoxPreference>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.moddedpa_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mContext = getActivity();
+        ContentResolver resolver = getActivity().getContentResolver();
 
         mUseAltResolver = (CheckBoxPreference) findPreference(PREF_USE_ALT_RESOLVER);
         mUseAltResolver.setChecked(Settings.System.getInt(

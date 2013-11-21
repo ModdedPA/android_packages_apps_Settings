b3940b8eb39974be5a211be9299b8477a532d352
David96 <xd_e-mail@gmx.de>
2013-11-21 22:24:17 +0100

    Revert "Alternate default app picker (2/2)"
    This reverts commit 025b407ff45d45d9f272e2c6426a278af1517553.
    
    Conflicts:
    	res/values/pa_strings.xml
    	res/xml/moddedpa_settings.xml
    	src/com/android/settings/moddedpa/ModdedPASettings.java

--- src/com/android/settings/moddedpa/ModdedPASettings.java
+++ src/com/android/settings/moddedpa/ModdedPASettings.java
@@ -38,10 +38,6 @@ import java.util.ArrayList;
 public class ModdedPASettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";  
-
-    private CheckBoxPreference mUseAltResolver;
-
     private Context mContext;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
@@ -57,28 +53,11 @@ public class ModdedPASettings extends SettingsPreferenceFragment
         mContext = getActivity();
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mUseAltResolver = (CheckBoxPreference) findPreference(PREF_USE_ALT_RESOLVER);
-        mUseAltResolver.setChecked(Settings.System.getInt(
-                getActivity().getContentResolver(),
-                Settings.System.ACTIVITY_RESOLVER_USE_ALT, 0) == 1); 
-
     }
 
     @Override
     public void onResume() {
         super.onResume();
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mUseAltResolver) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.ACTIVITY_RESOLVER_USE_ALT,
-                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
-        }
-
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {

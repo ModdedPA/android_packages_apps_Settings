6a74d1b1f5062fede9b8cf428ad7209250dae669
David96 <xd_e-mail@gmx.de>
2013-11-23 01:44:47 +0100

    Fix power notification settings + remove duplicate
    But playing a tone then plugging power cable in/out still doesn't work so I guess it's a framework problem.

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -414,6 +414,16 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         } else if (preference == mVolumeAdustSound) {
             Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED,
                     mVolumeAdustSound.isChecked() ? 1 : 0);
+        } else if (preference == mPowerSounds) {
+            Settings.Global.putInt(getContentResolver(), 
+                    Settings.Global.POWER_NOTIFICATIONS_ENABLED, mPowerSounds.isChecked() ? 1 : 0);
+        } else if (preference == mPowerSoundsVibrate) {
+            Settings.Global.putInt(getContentResolver(), 
+                    Settings.Global.POWER_NOTIFICATIONS_VIBRATE, mPowerSoundsVibrate.isChecked() ? 1 : 0);
+        } else if (preference == mPowerSoundsRingtone) {
+            launchNotificationSoundPicker(REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE,
+                    Settings.Global.getString(getContentResolver(),
+                            Settings.Global.POWER_NOTIFICATIONS_RINGTONE));
         }
         return true;
     }
@@ -436,23 +446,6 @@ public class SoundSettings extends SettingsPreferenceFragment implements
             } else {
                 showDialogInner(DLG_SAFE_HEADSET_VOLUME);
             }
-        }
-        if (KEY_POWER_NOTIFICATIONS.equals(key)) {
-            Settings.Global.putInt(getContentResolver(),
-                    Settings.Global.POWER_NOTIFICATIONS_ENABLED,
-                    (Boolean) objValue ? 1 : 0);
-
-        }
-        if (KEY_POWER_NOTIFICATIONS_VIBRATE.equals(key)) {
-            Settings.Global.putInt(getContentResolver(),
-                    Settings.Global.POWER_NOTIFICATIONS_VIBRATE,
-                    (Boolean) objValue ? 1 : 0);
-
-        }
-        if (KEY_POWER_NOTIFICATIONS_RINGTONE.equals(key)) {
-            launchNotificationSoundPicker(REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE,
-                    Settings.Global.getString(getContentResolver(),
-                            Settings.Global.POWER_NOTIFICATIONS_RINGTONE));
         }
         return true;
     }
@@ -636,7 +629,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
             toneName = getString(R.string.power_notifications_ringtone_silent);
             toneUriPath = POWER_NOTIFICATIONS_SILENT_URI;
         }
-
+        
         mPowerSoundsRingtone.setSummary(toneName);
         Settings.Global.putString(getContentResolver(),
                 Settings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriPath);
